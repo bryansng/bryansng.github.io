5bbc7cdf2e5c5da17537d87d00aedf2a8b1227d4
Bryan Sng <sngby98@gmail.com>
2018-08-12 12:30:36 +0100

    Done Lazy-load - Left make it load only when scrolled upon

--- photography/js/getImages.js
+++ photography/js/getImages.js
@@ -11,48 +11,54 @@ var slides_parent = document.getElementById("slides_parent");
 var wrapper_parent = document.getElementById("gallery_parent");
 
 var order_num = 0;
-//db.collection("photography").orderBy("filename").get().then((querySnapshot) => {
 db.collection("photography").orderBy("time_uploaded", "asc").get().then((querySnapshot) => {
-	querySnapshot.forEach((doc) => {
-		// TODO: Wrap the entire thing in a promise.
-		// https://stackoverflow.com/questions/51007999/how-to-wait-until-all-data-is-downloaded-in-firebase-firestore-storage?answertab=votes#tab-top
-		data = doc.data();
-		
-		// For slides.
-		var new_slide = document.createElement("div");
-		new_slide.classList.add("slide");
-		new_slide.style.order = -order_num;
-		
-		// For gallery wrapper.
-		var new_wrapper = document.createElement("div");
-		new_wrapper.classList.add("gallery_wrapper");
-		new_wrapper.style.order = -order_num++;
-		// Neg because, the order is based on left to right. The most left is the first one. i.e., if there is -3, -2, -1. -3 will be the most left.
-		
-		slides_parent.insertBefore(new_slide, slides_parent.childNodes[0]); // insert into first node.
-		wrapper_parent.insertBefore(new_wrapper, wrapper_parent.childNodes[0]); // insert into first node.
-		
-		// Create a child reference to the file we want to download.
-		var imgRef = storageRef.child(data["filepath"]);
-		
-		// Get the download URL.
-		imgRef.getDownloadURL().then(function(url){
-			// Insert url into an <img> tag to "download".
-			// Slide.
-			var new_slide_img = document.createElement("img");
-			new_slide_img.src = url;
-			new_slide_img.classList.add("modal_img");
+	// querySnapshot returns only the doc objects.
+	// docs property returns the array of doc objects.
+	// map method exists only in arrays.
+	Promise.all(querySnapshot.docs.map((doc) => {
+		return new Promise((resolve, reject) => {
+			data = doc.data();
 			
-			// Gallery_wrapper.
-			var new_wrapper_img = document.createElement("img");
-			new_wrapper_img.src = url;
-			new_wrapper_img.classList.add("gallery_img");
-			new_wrapper_img.onclick = function() {handleClick(this);};
+			// For slides.
+			var new_slide = document.createElement("div");
+			new_slide.classList.add("slide");
+			new_slide.style.order = -order_num;
 			
-			// Adds a class to the image, depending if image is horizontal or vertical.
-			new_wrapper_img.onload = function() {
-				var width = new_wrapper_img.naturalWidth;
-				var height = new_wrapper_img.naturalHeight;
+			// For gallery wrapper.
+			var new_wrapper = document.createElement("div");
+			new_wrapper.classList.add("gallery_wrapper");
+			new_wrapper.style.order = -order_num++;
+			// Neg because, the order is based on left to right. The most left is the first one. i.e., if there is -3, -2, -1. -3 will be the most left.
+			
+			slides_parent.insertBefore(new_slide, slides_parent.childNodes[0]); // insert into first node.
+			wrapper_parent.insertBefore(new_wrapper, wrapper_parent.childNodes[0]); // insert into first node.
+			
+			// Create a child reference to the file we want to download.
+			var imgRef = storageRef.child(data["filepath"]);
+			
+			// Get the download URL.
+			imgRef.getDownloadURL().then(function(url){
+				// Insert url into an <img> tag to "download".
+				// Slide.
+				var new_slide_img = document.createElement("img");
+				new_slide_img.src = url;
+				new_slide_img.classList.add("modal_img");
+				
+				// Gallery_wrapper.
+				var new_wrapper_img = document.createElement("img");
+				new_wrapper_img.setAttribute("data-src", url);
+				new_wrapper_img.classList.add("gallery_img");
+				new_wrapper_img.onclick = function() {handleClick(this);};
+				
+				// Adds a class to the image, depending if image is horizontal or vertical.
+				var remote_img = new Image();
+				remote_img.addEventListener("load", function(){
+					console.log(this.naturalWidth + ' ' + this.naturalHeight);
+				});
+				remote_img.src = url;
+				
+				var width = remote_img.naturalWidth;
+				var height = remote_img.naturalHeight;
 				
 				if (width > height){
 					new_wrapper_img.classList.add("horizontal");
@@ -61,37 +67,26 @@ db.collection("photography").orderBy("time_uploaded", "asc").get().then((querySn
 					new_wrapper_img.classList.add("vertical");
 					new_slide_img.classList.add("vertical");
 				}
-			}
-			
-			new_slide.appendChild(new_slide_img);
-			new_wrapper.appendChild(new_wrapper_img);	// insert img into div.
-		/*
-		}).catch(function(error){
-			// A full list of error codes is available at,
-			// https://firebase.google.com/docs/storage/web/handle-errors
-			switch(error.code){
-				case 'storage/object_not_found':
-					// File doesn't exist.
-					console.log('Object not found');
-					break;
-				case 'storage/unauthorized':
-					// User doesn't have permission to access the object.
-					console.log('You do not have the righr permissions');
-					break;
-				case 'storage/canceled':
-					// User canceled the upload.
-					console.log('Canceled');
-					break;
-				case 'storage/unknown':
-					// Unknown error occurred, inspect the serve response.
-					console.log('Who knows...');
-					break;
-			}
-		*/
+				
+				new_slide.appendChild(new_slide_img);
+				new_wrapper.appendChild(new_wrapper_img);	// insert img into div.
+				
+				resolve(url);
+			});
+		});
+	})).then((querySnapshots) => {
+		// Removes preloader and displays actual gallery.
+		wrapper_parent.style.display = "flex";
+
+		var temp_wrapper_parent = document.getElementById("temp_wrapper_parent");
+		temp_wrapper_parent.style.display = "none";
+		
+		// Lazy-loading, set the src to its data-src after it is done downloading.
+		[].forEach.call(document.querySelectorAll(".gallery_img[data-src]"), function(img){
+			img.setAttribute('src', img.getAttribute("data-src"));
+			img.onload = function(){
+				img.removeAttribute("data-src");
+			};
 		});
 	});
 });
-wrapper_parent.style.display = "flex";
-
-var temp_wrapper_parent = document.getElementById("temp_wrapper_parent");
-temp_wrapper_parent.style.display = "none";
--- photography/js/getImages_(before trying lazy-load).js
+++ photography/js/getImages_(before trying lazy-load).js
@@ -6,20 +6,31 @@ var storage = firebase.storage();
 // Create a storage reference from our storage service.
 var storageRef = storage.ref();
 
-var parent = document.getElementById("gallery_parent"); // get parent.
+// Get parent.
+var slides_parent = document.getElementById("slides_parent");
+var wrapper_parent = document.getElementById("gallery_parent");
 
 var order_num = 0;
 //db.collection("photography").orderBy("filename").get().then((querySnapshot) => {
 db.collection("photography").orderBy("time_uploaded", "asc").get().then((querySnapshot) => {
 	querySnapshot.forEach((doc) => {
+		// TODO: Wrap the entire thing in a promise.
+		// https://stackoverflow.com/questions/51007999/how-to-wait-until-all-data-is-downloaded-in-firebase-firestore-storage?answertab=votes#tab-top
 		data = doc.data();
-			
-		var new_div = document.createElement("div");
-		new_div.classList.add("gallery_wrapper");
-		new_div.style.order = -order_num++;
+		
+		// For slides.
+		var new_slide = document.createElement("div");
+		new_slide.classList.add("slide");
+		new_slide.style.order = -order_num;
+		
+		// For gallery wrapper.
+		var new_wrapper = document.createElement("div");
+		new_wrapper.classList.add("gallery_wrapper");
+		new_wrapper.style.order = -order_num++;
 		// Neg because, the order is based on left to right. The most left is the first one. i.e., if there is -3, -2, -1. -3 will be the most left.
 		
-		parent.insertBefore(new_div, parent.childNodes[0]); // insert into first node.
+		slides_parent.insertBefore(new_slide, slides_parent.childNodes[0]); // insert into first node.
+		wrapper_parent.insertBefore(new_wrapper, wrapper_parent.childNodes[0]); // insert into first node.
 		
 		// Create a child reference to the file we want to download.
 		var imgRef = storageRef.child(data["filepath"]);
@@ -27,25 +38,33 @@ db.collection("photography").orderBy("time_uploaded", "asc").get().then((querySn
 		// Get the download URL.
 		imgRef.getDownloadURL().then(function(url){
 			// Insert url into an <img> tag to "download".
-			var new_img = document.createElement("img");
-			new_img.src = url;
-			new_img.classList.add("gallery_img");
+			// Slide.
+			var new_slide_img = document.createElement("img");
+			new_slide_img.src = url;
+			new_slide_img.classList.add("modal_img");
+			
+			// Gallery_wrapper.
+			var new_wrapper_img = document.createElement("img");
+			new_wrapper_img.src = url;
+			new_wrapper_img.classList.add("gallery_img");
+			new_wrapper_img.onclick = function() {handleClick(this);};
 			
 			// Adds a class to the image, depending if image is horizontal or vertical.
-			new_img.onload = function() {
-				var width = new_img.naturalWidth;
-				var height = new_img.naturalHeight;
+			new_wrapper_img.onload = function() {
+				var width = new_wrapper_img.naturalWidth;
+				var height = new_wrapper_img.naturalHeight;
 				
 				if (width > height){
-					new_img.classList.add("horizontal");
+					new_wrapper_img.classList.add("horizontal");
+					new_slide_img.classList.add("horizontal");
 				} else if (height > width){
-					new_img.classList.add("vertical");
+					new_wrapper_img.classList.add("vertical");
+					new_slide_img.classList.add("vertical");
 				}
 			}
 			
-			//new_img.onclick = handleClick(order_num);
-		
-			new_div.appendChild(new_img);	// insert img into div.
+			new_slide.appendChild(new_slide_img);
+			new_wrapper.appendChild(new_wrapper_img);	// insert img into div.
 		/*
 		}).catch(function(error){
 			// A full list of error codes is available at,
@@ -72,7 +91,7 @@ db.collection("photography").orderBy("time_uploaded", "asc").get().then((querySn
 		});
 	});
 });
-parent.style.display = "flex";
+wrapper_parent.style.display = "flex";
 
-var temp_parent = document.getElementById("temp_parent");
-temp_parent.style.display = "none";
+var temp_wrapper_parent = document.getElementById("temp_wrapper_parent");
+temp_wrapper_parent.style.display = "none";
